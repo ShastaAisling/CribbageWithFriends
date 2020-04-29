79e4788de29fad4d7dfb0f293f42c7ec2c1cd5ea
jakeit1111 <jwmax11@gmail.com>
2020-04-29 14:33:23 -0400

    Added deck and card classes. Removed duplicate image files. Updated gitignore to exclude .idea project files.

---
+++ Assets/Scripts/Objects/Card.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Suit
+{
+    Spade = 'S',
+    Diamond = 'D',
+    Club = 'C',
+    Heart = 'H'
+}
+
+public class Card : MonoBehaviour
+{
+    private int rank;
+    private Suit suit;
+
+    public Card(int rank, Suit suit)
+    {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public string GetRank()
+    {
+        switch (rank)
+        {
+            case 1:
+                return "A";
+            case 11:
+                return "J";
+            case 12:
+                return "Q";
+            case 13: 
+                return "K";
+            default:
+                return rank.ToString();
+        }
+    }
+
+    public Suit GetSuit()
+    {
+        return suit;
+    }
+
+    public string GetCardName()
+    {
+        return GetRank() + suit + ".jpg";
+    }
+}
---
+++ Assets/Scripts/Objects/Deck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    private List<Card> cards;
+
+    public Deck()
+    {
+        cards = new List<Card>();
+        Shuffle();
+    }
+
+    public void Shuffle()
+    {
+        cards.Clear();
+
+        for (int i = 1; i <= 13; i++)
+        {
+            cards.Add(new Card(i, Suit.Club));
+            cards.Add(new Card(i, Suit.Diamond));
+            cards.Add(new Card(i, Suit.Heart));
+            cards.Add(new Card(i, Suit.Spade));
+        }
+    }
+
+    public Card Deal()
+    {
+        Card dealtCard = cards[Random.Range(0, cards.Count)];
+        cards.Remove(dealtCard);
+        return dealtCard;
+    }
+}
